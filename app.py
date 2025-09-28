bb633787b1f57f0cc423fd31c9ed19ffb334e448
Denis <ponikarovskikh.den@mail.ru>
2025-09-28 18:04:35 +0300

    Исправление

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 from flask import Flask, render_template, request
 
+from process import calc_cost
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['get', 'post'])
 def hello():
-    return "Hello!"
+    message = ""
+    if request.method == "POST":
+        area = request.form.get("area")
+        cost = calc_cost(area)
+        message = f"Стоимость квартиры: {cost} руб."
+    return render_template("index.html", message=message)
 
-@app.route("/login/", methods=['GET', 'POST'])
+@app.route("/login/", methods=['get', 'post'])
 def login():
     message = "Место под сообщение"
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        if username != "123":
+            message = "Неправильное имя пользователя"
+        else:
+            message = f"Имя пользователя: {username}, пароль {password}"
     return render_template("login.html", message=message)
 
 if __name__ == "__main__":
